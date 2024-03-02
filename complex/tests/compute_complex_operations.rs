950f463f39ce718bbad427f0f1c3a3dacf5041a0
“eduardevs <jeseduardopi@gmail.com>
2024-03-02 19:33:32 +0100

    Clean

--- complex/tests/compute_complex_operations.rs
+++ complex/tests/compute_complex_operations.rs
@@ -18,17 +18,13 @@ mod test1 {
 
     #[test]
     fn complex_addition() {
-        // Define two complex numbers to add
         let complex1 = Complex { re: 1.5, im: 2.5 };
         let complex2 = Complex { re: 3.0, im: 4.0 };
 
-        // Perform the addition
         let result = complex1 + complex2;
 
-        // Define the expected result
         let expected = Complex { re: 4.5, im: 6.5 };
 
-        // Assert that the actual result matches the expected result
         assert_eq!(
             result, expected,
             "Complex addition did not produce the expected result."
@@ -39,7 +35,6 @@ mod test1 {
         let complex1 = Complex { re: 1.0, im: 2.0 };
         let complex2 = Complex { re: 3.0, im: 4.0 };
         let result = complex1 * complex2;
-        // Based on the formula: (a+bi)(c+di) = (ac-bd) + (ad+bc)i
         assert_eq!(result, Complex { re: -5.0, im: 10.0 });
     }
 }
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -416,8 +416,6 @@ mod tests {
 
             let (_real, _image) =
                 networking::get_complex_from_coordinates(&range, x, y, &resolution);
-            // Assert that the complex number is correctly calculated.
-            // The exact values will depend on your implementation of get_complex_from_coordinates
         }
         #[test]
         fn test_fractal_intensity() {
@@ -428,15 +426,11 @@ mod tests {
 
             let mandelbrot_fractal = fractal::Mandelbrot { /* Fields */ };
 
-            // Test Julia set at a specific point
             let intensity_julia = networking::iterate_julia(&julia_fractal, 0.0, 0.0, 100);
-            assert!(intensity_julia.count > 0.0); // Adjust assertion based on expected behavior
+            assert!(intensity_julia.count > 0.0);
 
-            // Similarly, test Mandelbrot set at a specific point
             let intensity_mandelbrot = networking::iterate_mandelbrot(0.0, 0.0, 100);
-            assert!(intensity_mandelbrot.count > 0.0); // Adjust assertion based on expected behavior
+            assert!(intensity_mandelbrot.count > 0.0);
         }
-
-        // Your tests will go here
     }
 }
